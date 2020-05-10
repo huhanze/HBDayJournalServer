bf28e1d9b548c1bed2c7805e0ef184a3130f7b52
Dylan.Hu <dylanhu0516@gmail.com>
2020-05-10 20:25:37 +0800

    修改mysql地址

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -23,7 +23,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Configure a SQLite database
     
-    let mysql = MySQLDatabase(config: MySQLDatabaseConfig(hostname: "127.0.0.1", port: 3306, username: "dylanhu", password: "123456", database: "test", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext))
+    let mysql = MySQLDatabase(config: MySQLDatabaseConfig(hostname: "0.0.0.0", port: 3306, username: "dylanhu", password: "123456", database: "test", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext))
 
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
