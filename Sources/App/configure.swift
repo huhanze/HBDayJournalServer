d8a6a440df300d293c6b2973c94624e615be1e60
DylanHu <dylanhu0516@gmail.com>
2020-05-11 13:48:40 +0800

    修改服务器配置信息

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,11 @@ import Authentication
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
+
+    var serverConfigure = NIOServerConfig.default()
+    serverConfigure.hostname = "0.0.0.0"
+    serverConfigure.port = 8001
+    services.register(serverConfigure)
     try services.register(FluentMySQLProvider())
 
     // Register routes to the router
@@ -23,7 +28,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Configure a SQLite database
     
-    let mysql = MySQLDatabase(config: MySQLDatabaseConfig(hostname: "0.0.0.0", port: 3306, username: "dylanhu", password: "123456", database: "test", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext))
+    let mysql = MySQLDatabase(config: MySQLDatabaseConfig(hostname: "106.12.13.162", port: 3306, username: "root", password: "deitxihc521", database: "day_journal", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext))
 
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
