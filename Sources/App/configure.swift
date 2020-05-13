3ec8d9429dbc00701ae4036f607129332966391b
Dylan.Hu <dylanhu0516@gmail.com>
2020-05-13 15:41:22 +0800

    封装处理

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,49 +1,9 @@
-import FluentMySQL
-import Vapor
-import Authentication
 
+
+import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-
-    var serverConfigure = NIOServerConfig.default()
-    serverConfigure.hostname = "0.0.0.0"
-    serverConfigure.port = 80
-    services.register(serverConfigure)
-    try services.register(FluentMySQLProvider())
-
-    // Register routes to the router
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
-
-    // Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    services.register(middlewares)
     
-    try services.register(AuthenticationProvider())
-
-    // Configure a SQLite database
-    
-    let mysql = MySQLDatabase(config: MySQLDatabaseConfig(hostname: "106.12.13.162", port: 3306, username: "root", password: "deitxihc521", database: "day_journal", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext))
-    
-//    let mysql = MySQLDatabase(config: MySQLDatabaseConfig(hostname: "localhost", port: 3306, username: "dylanhu", password: "123456", database: "test", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext))
-
-    // Register the configured SQLite database to the database config.
-    var databases = DatabasesConfig()
-    databases.add(database: mysql, as: .mysql)
-    services.register(databases)
-
-    // Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .mysql)
-    migrations.add(model: User.self, database: .mysql)
-    migrations.add(model: AccessToken.self, database: .mysql)
-    migrations.add(model: RefreshToken.self, database: .mysql)
-    migrations.add(model: UserInfo.self, database: .mysql)
-    migrations.add(model: Image.self, database: .mysql)
-    services.register(migrations)
+    try ConfigureManager.configure(&config, &env, &services)
 }
